58a12b464dee68dc7128984fd87817e9faf43ccb
James Newton-King <james@newtonking.com>
2023-02-23 21:37:13 +0800

    Add metrics factory

--- src/Middleware/Diagnostics/src/DeveloperExceptionPage/DeveloperExceptionPageMiddlewareImpl.cs
+++ src/Middleware/Diagnostics/src/DeveloperExceptionPage/DeveloperExceptionPageMiddlewareImpl.cs
@@ -113,7 +113,7 @@ internal class DeveloperExceptionPageMiddlewareImpl
                 return;
             }
 
-            _logger.UnhandledException(ex);
+            DiagnosticsTelemetry.ReportUnhandledException(_logger, context, ex);
 
             if (context.Response.HasStarted)
             {
